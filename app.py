394e54d43e385923de074e0074befc9ba44fc7b8
Gerard Keiser <gerardkeiser@gmail.com>
2022-08-03 15:47:13 -0500

    Add failing test

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 """News sentiment tracker"""
 
-from flask import flask
+from flask import Flask, redirect, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.exc import IntegrityError
+import os
+
+from models import db, connect_db, User, Headline, Source, Rewrite
+
+# from forms import RewriteForm
+
+app = Flask(__name__)
+
+# From https://help.heroku.com/ZKNTJQSK/why-is-sqlalchemy-1-4-x-not-connecting-to-heroku-postgres
+uri = os.environ.get("DATABASE_URL", "postgresql:///headlines_dev")
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET")
+
+if os.getenv("FLASK_ENV") == "development":
+    from flask_debugtoolbar import DebugToolbarExtension
+
+    toolbar = DebugToolbarExtension(app)
+    app.config["SQLALCHEMY_ECHO"] = True
+
+connect_db(app)
+db.create_all()
