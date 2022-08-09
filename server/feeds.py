9b87b9b1e3043a32f7227137225ff36a07602e63
Gerard Keiser <gerardkeiser@gmail.com>
2022-08-09 07:22:44 -0500

    Functions for NYTimes headline import

---
+++ server/feeds.py
@@ -0,0 +1,35 @@
+import requests
+import xml.etree.ElementTree as ET
+from datetime import datetime
+
+from server.models import new_headline, db
+
+
+def get_nytimes_headlines():
+    nytimes = "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+    res = requests.get(nytimes)
+    root = ET.fromstring(res.text)
+    items = root.findall("./channel/item")
+    data = [
+        {
+            "text": i.find("title").text,
+            "url": i.find("link").text,
+            "date": parse_nytimes_date(i.find("pubDate").text),
+        }
+        for i in items
+    ]
+    return data
+
+
+def parse_nytimes_date(datestring: str | None) -> datetime | None:
+    if datestring:
+        return datetime.strptime(datestring, "%a, %d %b %Y %H:%M:%S %z")
+    else:
+        return None
+
+
+def send_to_database(source):
+    data = get_nytimes_headlines()
+    for item in data:
+        db.session.add(new_headline(**item, source_id=source.id))
+    db.session.commit()
